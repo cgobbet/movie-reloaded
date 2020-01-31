39fe84bdc6d7dde1ea5a8a81725c0dd663b70979
cgobbet <cassiano.gobbet@gmail.com>
2020-01-31 20:45:40 +0100

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,9 @@
+import MainView  from "./components/main-view/main-view";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
-import { Provider } from "react-redux";
-
-import MainView  from "./components/main-view/main-view";
 import moviesApp from "./reducers/reducers";
-
-// Import statement to indicate that we need to bundle `./index.scss`
-import "./index.scss";
 
 const store = createStore(moviesApp);
 
